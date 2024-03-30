44df559ab72b2f4b305cdc04fa74dd05ad1f1936
Pedro Rego <pedro.rego@stark.dk>
2024-03-30 11:52:08 +0000

    a

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -8,6 +8,8 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
   Pressable,
+  Dimensions,
+  FlatList,
 } from "react-native";
 import LoginForm from "./LoginForm";
 import { useState } from "react";
@@ -16,6 +18,88 @@ import Info from "./Info";
 
 const Login = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const { width } = Dimensions.get("screen");
+  const MainCard = () => {
+    return (
+      <View
+        style={{
+          display: "flex",
+
+          height: "88%",
+          justifyContent: "center",
+          alignItems: "center",
+          marginHorizontal: 4,
+          borderRadius: 12,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5, // Android only
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            columnGap: 30,
+            marginTop: 60,
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                fontSize: 18,
+
+                fontWeight: "500",
+              }}
+            >
+              LOGIN
+            </Text>
+            <View
+              style={{
+                height: 3,
+                backgroundColor: "orangered",
+                borderRadius: 2,
+              }}
+            />
+          </View>
+          <View>
+            <Text
+              style={{
+                fontSize: 18,
+
+                fontWeight: "500",
+              }}
+            >
+              SIGNUP
+            </Text>
+            <View
+              style={{
+                height: 3,
+                backgroundColor: "white",
+                borderRadius: 2,
+              }}
+            />
+          </View>
+        </View>
+        <LoginForm />
+        <Text
+          style={{
+            borderBottomWidth: 2,
+            borderColor: "white",
+            fontWeight: "500",
+            color: "gray",
+          }}
+        >
+          Forgot Password?
+        </Text>
+      </View>
+    );
+  };
   return (
     <SafeAreaView style={{ backgroundColor: "orangered" }}>
       <StatusBar barStyle={"light-content"} />
@@ -23,83 +107,7 @@ const Login = () => {
         <View
           style={{ backgroundColor: "orangered", display: "flex", rowGap: 3 }}
         >
-          <View
-            style={{
-              display: "flex",
-
-              height: "88%",
-              justifyContent: "center",
-              alignItems: "center",
-              marginHorizontal: 4,
-              borderRadius: 12,
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 2,
-              },
-
-              shadowOpacity: 0.25,
-              shadowRadius: 3.84,
-              elevation: 5, // Android only
-            }}
-          >
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                columnGap: 30,
-                marginTop: 60,
-              }}
-            >
-              <View>
-                <Text
-                  style={{
-                    fontSize: 18,
-
-                    fontWeight: "500",
-                  }}
-                >
-                  LOGIN
-                </Text>
-                <View
-                  style={{
-                    height: 3,
-                    backgroundColor: "orangered",
-                    borderRadius: 2,
-                  }}
-                />
-              </View>
-              <View>
-                <Text
-                  style={{
-                    fontSize: 18,
-
-                    fontWeight: "500",
-                  }}
-                >
-                  SIGNUP
-                </Text>
-                <View
-                  style={{
-                    height: 3,
-                    backgroundColor: "white",
-                    borderRadius: 2,
-                  }}
-                />
-              </View>
-            </View>
-            <LoginForm />
-            <Text
-              style={{
-                borderBottomWidth: 2,
-                borderColor: "white",
-                fontWeight: "500",
-                color: "gray",
-              }}
-            >
-              Forgot Password?
-            </Text>
-          </View>
+          <MainCard />
           <Pressable onPress={() => setOpenModal(true)}>
             <View
               style={{
