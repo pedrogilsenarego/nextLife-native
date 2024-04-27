2752b45b7b40786de70a5b7330a8130d1333a14d
Pedro Rego <pedro.rego@stark.dk>
2024-04-27 21:00:36 +0100

    a

--- app/(tabs)/Options/index.tsx
+++ app/(tabs)/Options/index.tsx
@@ -1,4 +1,7 @@
+import { Card } from "@/components/Atoms/Card";
+import { Container } from "@/components/Atoms/Container";
 import { supabase } from "@/lib/supabase";
+import { BlurView } from "expo-blur";
 import { View, Text, Pressable } from "react-native";
 
 const Options = () => {
@@ -6,12 +9,38 @@ const Options = () => {
     await supabase.auth.signOut();
   };
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Pressable onPress={logout} style={{ marginTop: 30, marginBottom: 200 }}>
-        <Text style={{ color: "whitesmoke", fontSize: 20, fontWeight: "600" }}>
-          Logout
-        </Text>
-      </Pressable>
+    <View
+      style={{
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        rowGap: 30,
+      }}
+    >
+      <BlurView
+        intensity={50}
+        style={{
+          padding: 20,
+          margin: 16,
+
+          justifyContent: "center",
+          overflow: "hidden",
+          borderRadius: 20,
+        }}
+      >
+        <Pressable onPress={logout}>
+          <Text
+            style={{ color: "whitesmoke", fontSize: 20, fontWeight: "600" }}
+          >
+            Logout
+          </Text>
+        </Pressable>
+      </BlurView>
+      <View style={{ height: 100, width: "100%", paddingHorizontal: 4 }}>
+        <Container>
+          <Text>Teste</Text>
+        </Container>
+      </View>
     </View>
   );
 };
