c966d790ff84619746d00dea9f51fdbd87b3c1bd
Pedro Rego <pedro.rego@stark.dk>
2024-05-07 18:46:52 +0100

    a

--- app/(tabs)/MainComponents/MainCard/PieChartMain/PieChartMain.tsx
+++ app/(tabs)/MainComponents/MainCard/PieChartMain/PieChartMain.tsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { View, Text, Animated } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSharedValue, withTiming } from "react-native-reanimated";
 import RenderItem from "./RenderItem";
@@ -22,10 +22,15 @@ const PieChartMain = () => {
   const GAP = 0.004;
 
   const [data, setData] = useState<Data[] | null>(null);
+  const [dataIncomes, setDataIncomes] = useState<Data[] | null>(null);
   const [mode, setMode] = useState<"expenses" | "incomes">("expenses");
-
-  const decimals = useSharedValue<number[]>([0.1, 0.1, 0.8]);
-
+  const dataToRender = mode === "expenses" ? data : dataIncomes;
+  const decimalsExpenses = useSharedValue<number[]>([0.1, 0.1, 0.8, 0.2, 0.3]);
+  const decimalsIncomes = useSharedValue<number[]>([0.1, 0.1, 0.8, 0.2, 0.3]);
+  const opacityRef = useRef(new Animated.Value(0)).current;
+  const opacityRef2 = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnimIncomes = useRef(new Animated.Value(0)).current;
   const { getExpensesCategoriesPercentage, getIncomesCategoriesPercentage } =
     useMetrics();
 
@@ -51,52 +56,157 @@ const PieChartMain = () => {
   ];
 
   useEffect(() => {
-    generateData();
+    generateDataExpenses();
+    generateDataIncomes();
   }, [mode]);
 
-  const generateData = () => {
-    const rawData =
-      mode === "expenses"
-        ? getExpensesCategoriesPercentage()
-        : getIncomesCategoriesPercentage();
+  const generateDataExpenses = () => {
+    const rawData = getExpensesCategoriesPercentage();
+
     const generateDecimals = rawData.map(
       (category) => category.percentage / 100
     );
 
-    decimals.value = [...generateDecimals];
+    decimalsExpenses.value = [...generateDecimals];
 
     const arrayOfObjects = rawData.map((value, index) => ({
       category: value.category,
       percentage: value.percentage,
-      color: mode === "expenses" ? shadesOfRed[index] : shadesOfGreen[index],
+      color: shadesOfRed[index],
       amount: value.amount,
     }));
 
     setData(arrayOfObjects);
   };
 
+  const generateDataIncomes = () => {
+    const rawData = getIncomesCategoriesPercentage();
+
+    const generateDecimals = rawData.map(
+      (category) => category.percentage / 100
+    );
+
+    decimalsIncomes.value = [...generateDecimals];
+
+    const arrayOfObjects = rawData.map((value, index) => ({
+      category: value.category,
+      percentage: value.percentage,
+      color: shadesOfGreen[index],
+      amount: value.amount,
+    }));
+
+    setDataIncomes(arrayOfObjects);
+  };
+
   const handleOnSelected = useCallback((selected: any) => {
     setMode(selected);
   }, []);
 
+  useEffect(() => {
+    animateScale2();
+    animateScale();
+    animateScaleIncomes();
+  }, [mode]);
+
+  const animateScale = () => {
+    Animated.spring(scaleAnim, {
+      toValue: mode === "expenses" ? 0 : 1,
+
+      useNativeDriver: true,
+    }).start();
+  };
+  const animateScaleIncomes = () => {
+    Animated.spring(scaleAnimIncomes, {
+      toValue: mode === "expenses" ? 0 : 1,
+
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animateScale2 = () => {
+    Animated.spring(opacityRef, {
+      toValue: mode === "expenses" ? 0 : 1,
+
+      useNativeDriver: true,
+    }).start();
+    Animated.spring(opacityRef2, {
+      toValue: mode === "expenses" ? 0 : 1,
+
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const scaleInterpolate = scaleAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.3],
+  });
+
+  const scaleInterpolateIncomes = scaleAnimIncomes.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0.3, 1],
+  });
+
+  const opacityInterpolate = opacityRef.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0.1],
+  });
+
+  const animatedStyle = {
+    transform: [{ scale: scaleInterpolate }],
+  };
+
+  const animatedStyleIncomes = {
+    transform: [{ scale: scaleInterpolateIncomes }],
+  };
+
+  const opacityInterpolate2 = opacityRef2.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0.1, 1],
+  });
+
   return (
-    <View>
-      <View
-        style={{
-          width: RADIUS * 2,
-          height: RADIUS * 2,
-          marginTop: 10,
-        }}
+    <View style={{ position: "relative" }}>
+      <Animated.View
+        style={[
+          animatedStyleIncomes,
+          {
+            position: "absolute",
+            opacity: opacityInterpolate2,
+            width: RADIUS * 2,
+            height: RADIUS * 2,
+            marginTop: 10,
+          },
+        ]}
       >
         <PieChart
           radius={RADIUS}
           gap={GAP}
           strokeWidth={STROKE_WIDTH}
           outerStrokeWidth={OUTER_STROKE_WIDTH}
-          decimals={decimals}
-          colors={mode === "expenses" ? shadesOfRed : shadesOfGreen}
+          decimals={decimalsIncomes}
+          colors={shadesOfGreen}
         />
-      </View>
+      </Animated.View>
+      <Animated.View
+        style={[
+          animatedStyle,
+          {
+            opacity: opacityInterpolate,
+            width: RADIUS * 2,
+            height: RADIUS * 2,
+            marginTop: 10,
+          },
+        ]}
+      >
+        <PieChart
+          radius={RADIUS}
+          gap={GAP}
+          strokeWidth={STROKE_WIDTH}
+          outerStrokeWidth={OUTER_STROKE_WIDTH}
+          decimals={decimalsExpenses}
+          colors={shadesOfRed}
+        />
+      </Animated.View>
       <Container>
         <ArrayButtons
           buttons={["expenses", "incomes"]}
@@ -112,7 +222,7 @@ const PieChartMain = () => {
           marginTop: 10,
         }}
       >
-        {data?.map((item, index) => {
+        {dataToRender?.map((item, index) => {
           return item.percentage <= 0 ? null : (
             <RenderItem item={item} key={index} index={index} />
           );
