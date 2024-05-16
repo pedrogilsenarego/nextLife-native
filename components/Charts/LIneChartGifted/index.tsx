44006f7a90cf205748de564098b826ffcbf0b7f6
Pedro Rego <pedro.rego@stark.dk>
2024-05-16 18:39:58 +0100

    a

--- components/Charts/LIneChartGifted/index.tsx
+++ components/Charts/LIneChartGifted/index.tsx
@@ -1,4 +1,5 @@
-import { Dimensions } from "react-native";
+import { useRef } from "react";
+import { Dimensions, View } from "react-native";
 import { LineChart } from "react-native-gifted-charts";
 
 type DataType = {
@@ -16,17 +17,32 @@ type Props = {
 export const LineChartGifted = ({ data, data2, color1, color2 }: Props) => {
   const width = Dimensions.get("window").width - 12;
 
+  const minDataValue = Math.min(
+    ...data.map((item) => item.value),
+    ...(data2 ? data2.map((item) => item.value) : [])
+  );
+
+  // Round down the minimum value to the nearest multiple of 1000
+  const roundedOffset = Math.floor(minDataValue / 1000) * 1000;
+  const spacing = (width - 65) / (data.length - 1);
+
   return (
     <LineChart
       data={data}
+      yAxisTextStyle={{ fontSize: 10 }}
+      xAxisLabelTextStyle={{ fontSize: 10 }}
       data2={data2}
+      trimYAxisAtTop
       isAnimated
+      yAxisOffset={roundedOffset}
       animationDuration={1000}
+      initialSpacing={3}
       areaChart
-      spacing={(width - 70) / data.length}
-      thickness={1}
+      spacing={spacing}
+      thickness={1.5}
       yAxisColor={"transparent"}
       xAxisColor={"transparent"}
+      pointerConfig={{}}
       //curved
       hideRules
       width={width}
