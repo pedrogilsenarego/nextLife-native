4ec8200d08aa1d7f807836ebc2fefb09bda47374
Pedro Rego <pedro.rego@stark.dk>
2024-07-10 13:27:11 +0100

    a

--- app/(tabs)/MainComponents/MainCard/index.tsx
+++ app/(tabs)/MainComponents/MainCard/index.tsx
@@ -4,9 +4,11 @@ import React, { useState } from "react";
 import ExpensesTable from "./ExpensesTable";
 import { Card } from "@/components/Atoms/Card";
 import { CardFooter } from "@/components/Molecules/CardFooter";
+import useBusinesses from "@/hooks/useBusinesses";
 
 const MainCard = () => {
   const [amountToShow, setAmountToShow] = useState<number>(10);
+  const businesses = useBusinesses();
   const [selectedStatus, setSelectedStatus] = useState<
     "expenses" | "incomes" | "both"
   >("expenses");
@@ -24,35 +26,77 @@ const MainCard = () => {
 
   return (
     <Card footer>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        onScroll={handleScroll}
-        scrollEventThrottle={16}
-        style={{
-          marginHorizontal: 10,
-          borderRadius: 8,
-          paddingTop: 16,
-          position: "relative",
-          height: "100%",
-        }}
-      >
-        <Pressable>
-          <View>
-            <ChartInitial
-              selectedStatus={selectedStatus}
-              setSelectedStatus={setSelectedStatus}
-              setAmountToShow={setAmountToShow}
-            />
-          </View>
+      {(businesses.data?.length || 0) < 1 ? (
+        <View
+          style={{
+            paddingHorizontal: 20,
+            flex: 1,
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "gray",
+              marginTop: 20,
 
-          <View style={{ marginTop: 6 }}>
-            <ExpensesTable
-              amountToShow={amountToShow}
-              selectedStatus={selectedStatus}
-            />
-          </View>
-        </Pressable>
-      </ScrollView>
+              paddingTop: 10,
+              lineHeight: 20,
+            }}
+          >
+            Here the expenses and incomes can be observed using line or bar
+            charts. Start by creating a{" "}
+            <Text style={{ fontWeight: 800 }}>Business</Text> and add some{" "}
+            <Text style={{ fontWeight: 800 }}>Entries</Text>.
+          </Text>
+          <Text
+            style={{
+              textAlign: "center",
+              color: "gray",
+              paddingTop: 10,
+              paddingBottom: 20,
+
+              lineHeight: 20,
+            }}
+          >
+            Here it is possible to make compositions and filter by{" "}
+            <Text style={{ fontWeight: 800 }}>Business</Text> and others, while
+            obtaining several metrics are available to help understand the
+            charts. It is also possible to select a certain day or month, to
+            have a better visualization of the metrics from that specification.
+          </Text>
+        </View>
+      ) : (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          onScroll={handleScroll}
+          scrollEventThrottle={16}
+          style={{
+            marginHorizontal: 10,
+            borderRadius: 8,
+            paddingTop: 16,
+            position: "relative",
+            height: "100%",
+          }}
+        >
+          <Pressable>
+            <View>
+              <ChartInitial
+                selectedStatus={selectedStatus}
+                setSelectedStatus={setSelectedStatus}
+                setAmountToShow={setAmountToShow}
+              />
+            </View>
+
+            <View style={{ marginTop: 6 }}>
+              <ExpensesTable
+                amountToShow={amountToShow}
+                selectedStatus={selectedStatus}
+              />
+            </View>
+          </Pressable>
+        </ScrollView>
+      )}
     </Card>
   );
 };
