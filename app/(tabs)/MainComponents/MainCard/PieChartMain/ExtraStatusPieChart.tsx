81ffeb85fa7de86561c8579a3a051e46e9ac160a
Pedro Rego <pedrogilsenarego@gmail.com>
2024-09-23 19:11:05 +0100

    a

--- app/(tabs)/MainComponents/MainCard/PieChartMain/ExtraStatusPieChart.tsx
+++ app/(tabs)/MainComponents/MainCard/PieChartMain/ExtraStatusPieChart.tsx
@@ -8,77 +8,110 @@ export const ExtraStatusPieChart = () => {
       style={{
         alignItems: "flex-start",
         justifyContent: "flex-start",
-        width: "auto",
-        padding: 10,
+
         flex: 1,
         rowGap: 10,
       }}
     >
-      <View>
-        <Text
+      <View
+        style={{
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          rowGap: 10,
+
+          backgroundColor: Colors.pearlWhite,
+          padding: 10,
+          borderRadius: 6,
+        }}
+      >
+        <View>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 14,
+              color: theme === "light" ? "gray" : "whitesmoke",
+            }}
+          >
+            Groceries
+          </Text>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 22,
+              marginTop: -3,
+              fontWeight: 600,
+              color: "grey",
+            }}
+          >
+            + 13%/150€
+          </Text>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 12,
+              marginTop: -4,
+              fontWeight: 600,
+              color: "grey",
+            }}
+          >
+            goal: -13%/300€
+          </Text>
+        </View>
+        <View>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 14,
+              color: theme === "light" ? "gray" : "whitesmoke",
+            }}
+          >
+            Car
+          </Text>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 22,
+              marginTop: -3,
+              fontWeight: 600,
+              color: "grey",
+            }}
+          >
+            - 23%/200€
+          </Text>
+          <Text
+            style={{
+              textAlign: "left",
+              fontSize: 12,
+              marginTop: -4,
+              fontWeight: 600,
+              color: "grey",
+            }}
+          >
+            goal: -10%/233€
+          </Text>
+        </View>
+        <View
           style={{
-            textAlign: "left",
-            fontSize: 14,
-            color: theme === "light" ? "gray" : "whitesmoke",
+            flexDirection: "column",
+            justifyContent: "center",
+
+            width: "100%",
           }}
         >
-          Car
-        </Text>
-        <Text
-          style={{
-            textAlign: "left",
-            fontSize: 22,
-            marginTop: -3,
-            fontWeight: 600,
-            color: Colors.black,
-          }}
-        >
-          + 23%
-        </Text>
-      </View>
-      <View>
-        <Text
-          style={{
-            textAlign: "left",
-            fontSize: 14,
-            color: theme === "light" ? "gray" : "whitesmoke",
-          }}
-        >
-          Car
-        </Text>
-        <Text
-          style={{
-            textAlign: "left",
-            fontSize: 22,
-            marginTop: -3,
-            fontWeight: 600,
-            color: Colors.black,
-          }}
-        >
-          + 23%
-        </Text>
-      </View>
-      <View>
-        <Text
-          style={{
-            textAlign: "left",
-            fontSize: 14,
-            color: theme === "light" ? "gray" : "whitesmoke",
-          }}
-        >
-          Car
-        </Text>
-        <Text
-          style={{
-            textAlign: "left",
-            fontSize: 22,
-            marginTop: -3,
-            fontWeight: 600,
-            color: Colors.black,
-          }}
-        >
-          + 23%
-        </Text>
+          <Text style={{ color: "black", fontSize: 12, fontWeight: 600 }}>
+            Target Objectives
+          </Text>
+          <Text
+            style={{
+              color: "black",
+              fontSize: 35,
+              fontWeight: 600,
+              marginTop: -6,
+            }}
+          >
+            Soon
+          </Text>
+        </View>
       </View>
     </View>
   );
