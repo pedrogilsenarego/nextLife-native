385375fce23d6e2c3e0fb556400288ce35b0cd1d
Pedro Rego <pedrogilsenarego@gmail.com>
2024-09-24 18:04:48 +0100

    a

--- components/Molecules/ArrayButtons/index.tsx
+++ components/Molecules/ArrayButtons/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, View, Text, Animated, Easing } from "react-native";
+import { Pressable, View, Text, Animated } from "react-native";
 import { ArrayButtonsProps } from "./arrayButtons.type";
 import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { Colors, useTheme } from "@/providers/ThemeContext";
@@ -14,18 +14,56 @@ export const ArrayButtons: React.FC<ArrayButtonsProps<any>> = memo(
     );
     const { theme } = useTheme();
     const animationLeft = useRef(new Animated.Value(0)).current;
-    const animationWidth = useRef(new Animated.Value(buttonWidth[0])).current;
+    const animationWidth = useRef(new Animated.Value(0)).current;
 
+    // UseEffect for initial mount only
     useEffect(() => {
       if (buttonWidth[0] !== 0) {
-        Animated.spring(animationWidth, {
-          toValue: buttonWidth[0],
-          useNativeDriver: false,
-          speed: 1,
-          bounciness: 1,
-        }).start();
+        const selectedIndex = buttons.findIndex(
+          (button) => button === selectedStatus
+        );
+
+        if (buttonWidth[selectedIndex] !== undefined) {
+          const totalLeft = buttonWidth
+            .slice(0, selectedIndex)
+            .reduce((acc, width) => acc + width + GAP_BUTTONS, 0);
+
+          // Set initial position and width
+          animationLeft.setValue(totalLeft);
+          animationWidth.setValue(buttonWidth[selectedIndex]);
+        }
       }
     }, [buttonWidth]);
+
+    // UseEffect for handling updates after mount (user interaction)
+    useEffect(() => {
+      if (buttonWidth[0] !== 0) {
+        const selectedIndex = buttons.findIndex(
+          (button) => button === selectedStatus
+        );
+
+        if (buttonWidth[selectedIndex] !== undefined) {
+          const totalLeft = buttonWidth
+            .slice(0, selectedIndex)
+            .reduce((acc, width) => acc + width + GAP_BUTTONS, 0);
+
+          // Animate position and width on button change
+          Animated.spring(animationLeft, {
+            toValue: totalLeft,
+            useNativeDriver: false,
+            speed: 1,
+            bounciness: 1,
+          }).start();
+
+          Animated.spring(animationWidth, {
+            toValue: buttonWidth[selectedIndex],
+            useNativeDriver: false,
+            speed: 1,
+            bounciness: 1,
+          }).start();
+        }
+      }
+    }, [selectedStatus]);
 
     const handleLayout = (event: any, index: number) => {
       const newWidths = [...buttonWidth];
@@ -35,28 +73,10 @@ export const ArrayButtons: React.FC<ArrayButtonsProps<any>> = memo(
 
     const handlePress = useCallback(
       (button: any, index: number) => {
-        if (onSelected) onSelected(button as typeof button);
-        setSelectedStatus(button);
-
-        const totalLeft = buttonWidth
-          .slice(0, index)
-          .reduce((acc, width) => acc + width + GAP_BUTTONS, 0);
-
-        Animated.spring(animationLeft, {
-          toValue: totalLeft,
-          useNativeDriver: false,
-          speed: 1,
-          bounciness: 1,
-        }).start();
-
-        Animated.spring(animationWidth, {
-          toValue: buttonWidth[index],
-          useNativeDriver: false,
-          speed: 1,
-          bounciness: 1,
-        }).start();
+        if (onSelected) onSelected(button);
+        setSelectedStatus(button); // Set selected status to trigger the animation
       },
-      [onSelected, buttonWidth]
+      [onSelected]
     );
 
     return (
