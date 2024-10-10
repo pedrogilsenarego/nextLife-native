c2c5969e934a04aec364ed0605d1959c487185c2
Pedro Rego <pedrogilsenarego@gmail.com>
2024-10-10 19:16:33 +0100

    a

--- app/(tabs)/MainComponents/Header/index.tsx
+++ app/(tabs)/MainComponents/Header/index.tsx
@@ -1,7 +1,7 @@
 import useMetrics from "@/hooks/useMetrics";
 import useUser from "@/hooks/useUser";
 import { Colors, useTheme } from "@/providers/ThemeContext";
-import { AntDesign, Entypo } from "@expo/vector-icons";
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
 import { View, Text, Dimensions, Pressable } from "react-native";
 import { StateSelecter } from "./StateSelecter";
 import { SharedValue } from "react-native-reanimated";
@@ -10,6 +10,7 @@ import useIncomes from "@/hooks/useIncomes";
 import { dateRangeLabel } from "@/mappers/dateRange";
 import { useApp } from "@/providers/AppProvider";
 import { useMemo } from "react";
+import Skeleton from "@/components/Atoms/Skeleton";
 
 type Props = {
   setSideMenu: (sideMenu: boolean) => void;
@@ -32,16 +33,16 @@ export const Header = ({ setSideMenu, setSideLeftMenu }: Props) => {
   const width = Dimensions.get("screen").width;
   const loading = expenses.isLoading || incomes.isLoading;
   const balance = useMemo(() => {
-    return (totalIncomes() - totalExpenses()).toFixed(1);
+    return (totalIncomes() - totalExpenses()).toFixed(0);
   }, [totalIncomes, totalExpenses]);
 
   return (
     <View
       style={{
-        flexDirection: "row",
+        flexDirection: "column",
         justifyContent: "space-between",
-        paddingHorizontal: 12,
-        paddingVertical: 6,
+        paddingHorizontal: 4,
+        paddingVertical: 4,
         marginHorizontal: 10,
 
         backgroundColor: mainColor,
@@ -58,96 +59,121 @@ export const Header = ({ setSideMenu, setSideLeftMenu }: Props) => {
         elevation: 5,
       }}
     >
-      <View style={{ rowGap: 2 }}>
-        <Text
-          style={{
-            color: "white",
-            fontSize: 20,
-          }}
-        >
-          Hello,{" "}
-          <Text
-            style={{
-              fontWeight: "bold",
-              textTransform: "capitalize",
-            }}
-          >
-            {userQuery?.data?.username}
-          </Text>
-        </Text>
-        <Text
-          style={{
-            fontSize: 12,
-            color: "whitesmoke",
-          }}
-        >
-          Your {dateRangeLabel(dateRange)} balance
-        </Text>
-        {!loading && (
-          <Text
-            style={{
-              color: "white",
-              fontSize: 18,
-              fontWeight: "bold",
-            }}
-          >
-            {balance} Є
-          </Text>
-        )}
-      </View>
       <View
         style={{
           display: "flex",
-          flexDirection: "column",
-          rowGap: 2,
+          flexDirection: "row",
+          columnGap: 6,
           justifyContent: "space-between",
-          alignItems: "flex-end",
         }}
       >
-        <Text
+        <Pressable
+          onPress={() => setSideLeftMenu(true)}
           style={{
-            marginTop: 6,
-            color: "whitesmoke",
-          }}
-        >
-          {formattedDate}
-        </Text>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            columnGap: 5,
-            marginTop: 6,
+            borderRadius: 4,
             justifyContent: "center",
-            alignItems: "center",
+
+            zIndex: 10,
+            backgroundColor: mainColor,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
           }}
         >
-          <Pressable
+          <Ionicons size={25} name="chevron-back" color={Colors.pearlWhite} />
+        </Pressable>
+        <View style={{ flex: 1 }}>
+          <View
             style={{
-              padding: 10,
-              backgroundColor: "transparent",
-              borderRadius: 30,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "baseline",
             }}
-            onPress={() => setSideLeftMenu(true)}
           >
-            <Entypo
-              size={26}
-              name="add-to-list"
-              color={Colors.lightGray}
-              style={{ padding: -4 }}
-            />
-          </Pressable>
-          <Pressable
-            style={{
-              padding: 10,
-              backgroundColor: "transparent",
-              borderRadius: 30,
-            }}
-            onPress={() => setSideMenu(true)}
-          >
-            <AntDesign size={20} name="menu-fold" color={Colors.lightGray} />
-          </Pressable>
+            <View style={{ rowGap: 2 }}>
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 18,
+                  fontWeight: "bold",
+                  textTransform: "capitalize",
+                }}
+              >
+                {userQuery?.data?.username}
+              </Text>
+            </View>
+            <Text
+              style={{
+                marginTop: 6,
+                color: "whitesmoke",
+              }}
+            >
+              {formattedDate}
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={{
+                fontSize: 12,
+                color: "whitesmoke",
+              }}
+            >
+              Your {dateRangeLabel(dateRange)} balance
+            </Text>
+            {!loading ? (
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 18,
+                  fontWeight: "bold",
+                }}
+              >
+                {balance}
+                <Text style={{ fontSize: 14 }}>Є</Text>
+              </Text>
+            ) : (
+              <Skeleton
+                height={18}
+                style={{
+                  marginTop: 1.8,
+                  backgroundColor: `${Colors.pearlWhite}66`,
+                }}
+                width={70}
+              />
+            )}
+          </View>
         </View>
+        <Pressable
+          onPress={() => setSideMenu(true)}
+          style={{
+            borderRadius: 4,
+            justifyContent: "center",
+
+            zIndex: 10,
+            backgroundColor: mainColor,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }}
+        >
+          <Ionicons
+            size={25}
+            name="chevron-forward"
+            color={Colors.pearlWhite}
+          />
+        </Pressable>
       </View>
     </View>
   );
